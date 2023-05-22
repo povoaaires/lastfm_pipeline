2641d0996c021ab9f0dba458f0ce7fa3f76c6d6f
Benedito Povoa <dito.povoa@gmail.com>
2023-05-22 19:37:35 -0300

    create functions and enviromnents variables

--- src/exctract/api-load-raw.py
+++ src/exctract/api-load-raw.py
@@ -1,24 +1,74 @@
 import requests
+import os
+from dotenv import load_dotenv
 import json
 
 
-API_KEY = '925252b2baf7b11b361e8569bf9cafab'
-USER_AGENT = 'app_project'
 
-headers = {
-    'user-agent': USER_AGENT
-}
+load_dotenv()
 
-payload = {
-    'api_key': API_KEY,
-    'method': 'chart.gettopartists',
-    'format': 'json'
-}
+key_api = os.getenv("api_key")
+app_api = os.getenv("app_project")
+url_api = os.getenv("url_api")
+path = os.getenv("path")
 
-r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
-print(r)
+methods_general = ['chart.gettopartists','chart.gettoptracks','chart.gettoptags']
+methods_user = ['chart.gettopartists','chart.gettoptracks','chart.gettoptags']
 
 
-j = r.json()
-text = json.dumps(j, sort_keys=True, indent=4)
-print(text)
+def ExtractGeneral(general, url, app, key):
+    
+    headers = {
+        'user-agent': app
+    }
+    
+    body = {
+        'api_key': key,
+        'method': general,
+        'format': 'json'
+    }
+    
+    try:
+        request = requests.get(url, headers=headers, params=body)
+    except Exception as ex:
+        print(ex)
+    
+    return request
+
+
+def ExtractUser(user_method,user, url, app, key):
+    
+    headers = {
+        'user-agent': app
+    }
+    
+    body = {
+        'api_key': key,
+        'method': user_method,
+        'user': user,
+        'format': 'json'
+    }
+    try:
+        request = requests.get(url, headers=headers, params=body)
+    except Exception as ex:
+        print(ex)
+        
+    return request
+    
+
+
+
+r = ExtractGeneral(methods_general[0], url_api, app_api, key_api)
+
+artistas = r.json()
+artistas_exctraction = artistas['artists']['artist']
+#json_objeto = json.dumps(artistas_exctraction,ident=4)
+with open(f"{path}artistas.json", "w") as file:
+    json.dump(artistas_exctraction,file)
+    
+    
+#print(artistas_exctraction)
+
+
+# text = json.dumps(j, sort_keys=True, indent=4)
+# print(text)
